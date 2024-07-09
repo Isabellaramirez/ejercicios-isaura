000df6e0396e22dda0144e1c6bbfb0b992116a41
Isabellaramirez <150964832+Isabellaramirez@users.noreply.github.com>
2024-07-09 12:53:16 -0500

    Create pantalon.py

---
+++ pantalon.py
@@ -0,0 +1,10 @@
+pantalon=75000
+camisa=35000
+celular= 900000
+porcentaje= 14/100
+tienda_azul= (pantalon + camisa) 
+totalporcentaje= tienda_azul * porcentaje
+totaldescuento= tienda_azul-totalporcentaje
+print ("el total de la cuenta de tienda azul con el descuento del 14% es:", totaldescuento)
+total_Dia_de_compras= totaldescuento + celular
+print("El señor Rafael se gasto en su dia de compras un total de:", total_Dia_de_compras)
