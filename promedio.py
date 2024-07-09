e74a1d195e8dbd475976386624b962a313bc6bf4
Isabellaramirez <150964832+Isabellaramirez@users.noreply.github.com>
2024-07-09 12:52:53 -0500

    Create promedio.py

---
+++ promedio.py
@@ -0,0 +1,8 @@
+print("ingrese las 5 notas de este periodo de la asignatura de ingles:")
+nota1=float(input("primera nota:"))
+nota2=float(input("segunda nota"))
+nota3=float(input("tercera nota"))
+nota4=float(input("cuarta nota"))
+nota5=float(input("quita nota"))
+promedio= (nota1 + nota2 + nota3 + nota4 + nota5) / 5
+print("Su promedio de este periodo de la asignatura de ingles es:", promedio)
